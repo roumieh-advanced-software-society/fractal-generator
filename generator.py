c2ece9001dd7fb61ad5e00100ef98bb74598d62f
CharbelAD <abidahercharbel@gmail.com>
2020-10-23 20:39:38 +0300

    Improved performance and memory usage

--- generator.py
+++ generator.py
@@ -3,30 +3,10 @@ import matplotlib.pyplot as plt
 import multiprocess as mp
 import time
 import numpy as np
-import tqdm   # For the progress bar, no idea what is stands for
+import tqdm
 import cmath  # Complex math library
 from math import sqrt
-
-
-def get_size(obj, seen=None):
-    """Recursively finds size of objects"""
-    size = sys.getsizeof(obj)
-    if seen is None:
-        seen = set()
-    obj_id = id(obj)
-    if obj_id in seen:
-        return 0
-    # Important mark as seen *before* entering recursion to gracefully handle
-    # self-referential objects
-    seen.add(obj_id)
-    if isinstance(obj, dict):
-        size += sum([get_size(v, seen) for v in obj.values()])
-        size += sum([get_size(k, seen) for k in obj.keys()])
-    elif hasattr(obj, '__dict__'):
-        size += get_size(obj.__dict__, seen)
-    elif hasattr(obj, '__iter__') and not isinstance(obj, (str, bytes, bytearray)):
-        size += sum([get_size(i, seen) for i in obj])
-    return size
+import PIL
 
 
 def canvas2point(row, column, canvas_size, center, zoom):
@@ -56,7 +36,7 @@ def time2escape(c, limit, used_function):
     For anything that takes more than a limit number of steps, it
     will be considered 'locked' inside (the function will return
     the limit)
-    (Black for most representations)"""
+    """
     i = 0
     z = 0
     while sqrt(abs(z)) < 2:
@@ -74,7 +54,10 @@ def empty_canvas(canvas_size):
     values as int8 (C-like array) which means that a limit of more than
     127 is not allowed!
     canvas_size = (# of rows, # of columns)"""
+
+    # Currently unused
     return np.zeros(canvas_size, dtype="int8")
+
 
 
 def mandel(canvas_size=(100, 100), limit=30, center=(0.5, 0), zoom=1, used_function=f, dpi=300):
@@ -82,20 +65,18 @@ def mandel(canvas_size=(100, 100), limit=30, center=(0.5, 0), zoom=1, used_funct
     """This is the main function that will draw the image
     Format of canvas_size: (# of rows, # of columns)
     Format of center: (x coordinate, y coordinate)"""
+    
     #image = empty_canvas(canvas_size)  # Create a canvas to draw on
-
-
-    # TODO: Modify the 'image' variable directly instead of creating a return value and then putting it in image to
-    # save memory. (Should cut the usage by half, theoretically)
 
     def row_transform(row_id):  # To be used for mapping in the pool below (for multiprocessing)
         # type: (int) -> list
         """Transform a given row (expected to be a row of an image that is an empty canvas.
         Notice the use of row_id instead of row. This allows to create an empty canvas and
         still map over it correctly"""
-        row = image[row_id]
+        # TODO: Allocate all the memory beforehand and change in-place
+        row = np.zeros(canvas_size[1], dtype='int8')
         for i in range(0, canvas_size[1]):
-            step = time2escape(canvas2point(float(row_id), float(i), canvas_size, center, zoom), limit, used_function)
+            step = time2escape(canvas2point(row_id, i, canvas_size, center, zoom), limit, used_function)
             row[i] = limit - step
         return row
 
@@ -104,26 +85,47 @@ def mandel(canvas_size=(100, 100), limit=30, center=(0.5, 0), zoom=1, used_funct
     start = time.time()
 
     # Transform the image while showing a progress bar
-
-    image = list(tqdm.tqdm(pool.imap(row_transform, range(0, canvas_size[0]), chunksize=100), total=canvas_size[0]))
-
+    image_generator = tqdm.tqdm(pool.imap(row_transform, range(0, canvas_size[0]), chunksize=100), total=canvas_size[0])
+    image = np.empty(canvas_size)
+    for i, row in enumerate(image_generator): 
+        image[i] = row
     # NOTE: In case of not using full CPU power, increase chunksize until enough power is used!
     #       In case of errors with 'cannot join, blah blah blah', you must reduce chunksize
 
     print("Calculations took %d seconds" % (time.time() - start))
 
+
     print("Saving to 'fractal.png'...")
-    plt.imshow(image)
     start = time.time()
-    plt.savefig("fractal.png", dpi=dpi, frameon=False, bbox_inches='tight', pad_inches=0)
+    img = PIL.Image.fromarray(image)
+    img = img.convert('P')
+    img.putpalette([0,0,0,
+		31, 119, 180,
+		255, 127, 14,
+		44, 160, 44,
+		214, 39, 40,
+		148, 103, 189,
+		140, 86, 75,
+		227, 119, 194,
+		127, 127, 127,
+		188, 189, 34,
+		23, 190, 207])
+    # img.show()
+    img.save('fractal.png')
+    """
+    # Old code using matplotlib: (beware, much slower to save)
+    plt.imshow(image)
     # Extra arguments are to reduce wasted space outside of the image
+    plt.savefig("fractal.png", dpi=dpi, frameon=False, bbox_inches='tight', pad_inches=0)
+    """
     print("Saving took %d seconds" % (time.time() - start))
 
 
 # Application
-mandel(canvas_size=[5000, 5000], limit=30, zoom=0.5, center=[0, 0],
-       used_function=lambda z, c: cmath.exp(z*c+c), dpi=2000)
-
+#mandel(canvas_size=[5000, 5000], limit=30, zoom=0.5, center=[0, 0],
+#       used_function=lambda z, c: cmath.exp(z*c+c), dpi=2000)
+mandel(canvas_size=[5000, 5000], limit=30, zoom=1, center=[0.75, 0],
+       used_function=f, dpi=2000)
 
 # NOTE: if you increase canvas_size too much, you have to increase dpi to match it, otherwise, you'll be computing
 #       things that will not appear in the image, which is a waste of time. Check actual image size to see if it has
